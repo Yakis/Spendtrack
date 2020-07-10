74331a236d98fe1cc30150479cf101eaceb66c14
Mugurel Moscaliuc <mugur.moscaliuc@gmail.com>
2020-07-10 19:26:48 +0100

    Initial Commit

---
+++ Shared/SpendtrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendtrackApp.swift
+//  Shared
+//
+//  Created by Mugurel Moscaliuc on 10/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct SpendtrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
